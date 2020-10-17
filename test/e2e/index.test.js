cb1375b6758dba7baf51fcb6a665a61f47a238f2
Fabrice Lamant <flamant@shutterstock.com>
2020-10-17 19:34:11 +0100

    add e2e test

---
+++ test/e2e/index.test.js
@@ -0,0 +1,30 @@
+const gremlin = require('../..');
+
+const connect = (host, port, opts) => new Promise((resolve, reject) => {
+  const graph = new gremlin.structure.Graph();
+  const connection = new gremlin.driver.AwsSigV4DriverRemoteConnection(
+    host, port, opts,
+    () => { resolve({ connection, g: graph.traversal().withRemote(connection) }); },
+    (code, message) => { reject(new Error({ code, message })); },
+    (err) => { reject(err); },
+  );
+});
+
+test('connection', async () => {
+  const { connection, g } = await connect(
+    process.env.TEST_HOST,
+    process.env.TEST_PORT,
+    {
+      accessKeyId: process.env.TEST_ACCESS_KEY_ID,
+      secretAccessKey: process.env.TEST_SECRET_ACCESS_KEY,
+      region: process.env.TEST_REGION,
+      secure: process.env.TEST_SECURE === 'true',
+    },
+  );
+  const result = await g.V().count().next();
+  connection.close();
+  expect(result).toMatchObject({
+    value: expect.any(Number),
+    done: expect.any(Boolean),
+  });
+});
